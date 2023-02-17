bb3cec477357ad55bd10b24683358608a7ca9b3d
Pordrack <50073154+Pordrack@users.noreply.github.com>
2023-02-17 01:58:17 -0500

    Ajout de contenu

--- highlightedProjects.js
+++ highlightedProjects.js
@@ -53,6 +53,54 @@ let projects=[
             "Après cela j'ai ajouté un module permettant de réserver et consulter ses repas à la cantine, ainsi que d'échanger ses repas entre collègues en cas de désistement/besoin de dernière minute."
         ]
     },
+
+    {
+        'name':'[Prototype] Callback',
+        'date':'2022-12-19',
+        
+        'image':'img/highlightedProjects/callback.webp',
+        'description':[
+            "Réalisé en équipe de 7 dans le cadre d'un cours d'université, il s'agit d'un prototype de jeu d'Escape Game  sur le thème de voyage dans le temps fait sur Unreal Engine 5. Le joueur peut y faire des actions de différents types.",
+            "Je me suis occupé de toute la partie réseau/multijoueurs. J'ai d'abord créé une API <a href=\"https://fr.wikipedia.org/wiki/Representational_state_transfer\">REST</a> en <a href=\"https://fr.wikipedia.org/wiki/Node.js\">Node.Js</a> permettant aux joueurs de choisir un pseudo puis de voir leurs actions et temps enregistrés. Ces données servent ensuite à montrer aux utilisateurs les choix et temps de leurs pairs (regroupés par type d'actions). J'ai ensuite intégré l'API au jeu en utilisant du C++ et du Blueprint."
+        ]
+    },
+
+    {
+        'name':'Language automata',
+        'date':'2022-03-19',
+        'link':'https://pordrack.github.io/languageAutomate/',
+        'sourceCode':'https://github.com/Pordrack/languageAutomate',
+        'tinyimg':true,
+
+        'image':'img/highlightedProjects/automata.png',
+        'description':[
+            "Cette application web programmée en JavaScript from scratch génère des <a href=\"https://fr.wikipedia.org/wiki/Automate_fini_d%C3%A9terministe\">automates finis déterministes</a> à partir d'<a href=\"https://fr.wikipedia.org/wiki/Expression_r%C3%A9guli%C3%A8re\">expressions régulières</a> entrées par l'utilisateur. Il peut ensuite écrire des mots et voir l'automate les analyser en direct.",
+        ]
+    },
+
+    {
+        'name':'An Endless Search',
+        'date':'2021-01-19',
+        'link':'https://pordrack.itch.io/an-endless-search',
+        'sourceCode':'https://www.lexaloffle.com/bbs/?tid=41339',
+        'tinyimg':true,
+
+        'image':'img/highlightedProjects/endless.png',
+        'description':[
+            "Réalisé seul et en 48h pour la Global Game Jam 2021 (thème \"Lost and Food\"), ce jeu  inspiré par l'<a href=\"https://www.abandonware-france.org/ltf_abandon/ltf_jeu.php?id=1696\">Arche du Captain Blood</a> propose de parcourir un univers généré pour croiser les informations des PNJ et retrouver la planète de son parent. Il a été programmé en utilisant le moteur <a href=\"https://fr.wikipedia.org/wiki/Pico-8\">Pico 8</a> qui possède des restrictions inspirées par l'ère 8 bits.",
+        ]
+    },
+
+    {
+        'name':'[Prototype] Eliatrope',
+        'date':'2020-05-19',
+        'link':'https://pordrack.itch.io/abandoned-prototype-eliatrope',
+
+        'image':'img/highlightedProjects/eliatrope.png',
+        'description':[
+            "Réalisé seul, ce jeu de plateforme basé sur l'univers du MMORPG <a href=\"https://fr.wikipedia.org/wiki/Wakfu_(jeu_vid%C3%A9o)\">Wakfu</a> (dont il est un fangame) permet au joueur de courir, sauter, placer des portails de téléportation et attaquer des monstres. Un système de création/utilisation de macros pour les placements de portails est également présent. Le jeu a été réalisé sur le moteur minimaliste <a href=\"https://fr.wikipedia.org/wiki/L%C3%96VE\">Love2D</a>. Sa physique a donc été programmée from scratch.",
+        ]
+    },
 ]
 
 let i=0;
@@ -61,6 +109,9 @@ for (let project of projects) {
     let projectBoard=document.createElement("div");
     projectBoard.classList.add("highlightProject-container");
     projectBoard.classList.add((i%2)?"even":"odd");
+    if(project.tinyimg==true){
+        projectBoard.classList.add("tinyimg")
+    }
     i++;
     
     //D'abord le header
@@ -119,7 +170,7 @@ for (let project of projects) {
 
     if(!emptyLinks){
         projectBoard.appendChild(links);
-    }
+    }   
 
     //Et les paragraphes de sa description (si il en a une)
     if(project.description!=null){
